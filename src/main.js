5ff5c22fd8f1c880e4fd2d1e13d3580fb25dccc5
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-28 13:04:49 +0200

    clerk plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { pinia } from './store'
-import { Clerk } from '@clerk/vue';
+import { ClerkPlugin } from '@clerk/vue' 
 
 
 const app = createApp(App)
@@ -10,7 +10,7 @@ const app = createApp(App)
 app.use(router)
 app.use(pinia)
 
-app.use(Clerk, {
+app.use(ClerkPlugin, {
   publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
   frontendApi: import.meta.env.VITE_CLERK_FRONTEND_API
 })
