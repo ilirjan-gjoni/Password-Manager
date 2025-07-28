58d76410685c542bbea006027e45bbee941371b5
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-28 14:30:40 +0200

    build

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { clerkPlugin } from '@clerk/vue'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
+  throw new Error('Add your Clerk Publishable Key to the .env file ')
 }
 
 const app = createApp(App)
