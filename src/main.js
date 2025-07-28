1c890c1e60af05c6a10547d7015bce00a557ca82
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-28 13:08:44 +0200

    change of clerk plugin

--- src/main.js
+++ src/main.js
@@ -2,17 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { pinia } from './store'
-import { ClerkPlugin } from '@clerk/vue' 
+import { clerkPlugin } from '@clerk/vue' 
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
 
-app.use(ClerkPlugin, {
-  publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
-  frontendApi: import.meta.env.VITE_CLERK_FRONTEND_API
-})
-
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
 app.mount('#app')
