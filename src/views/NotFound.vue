6b501f68d7ab94c52dc564196608415d3773e56e
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-29 18:49:30 +0200

    notfound

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,14 +1,23 @@
 <script setup>
+import { useAuth } from '@clerk/vue'
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+
+const { isLoaded, isSignedIn } = useAuth()
+const router = useRouter()
+
+onMounted(() => {
+  if (!isLoaded.value) return
+  if (!isSignedIn.value) {
+    router.replace('/sign-in')
+  }
+})
 </script>
 
-
 <template>
-  <Protect redirectTo="/sign-in">
-  <div>
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-    <router-link to="/add">Go to Add Password</router-link>
+  <div v-if="isSignedIn && isLoaded" class="p-10 text-center">
+    <h1 class="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+    <router-link to="/add" class="text-blue-500 underline">Go to Add Password</router-link>
   </div>
-  </Protect>
 </template>
 
