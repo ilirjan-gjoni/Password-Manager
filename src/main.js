184e24d819161767880f537028bbf6e2a85f9598
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-29 15:38:47 +0200

    clerk

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import { pinia } from './store'
+import { clerkPlugin } from '@clerk/vue'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 const app = createApp(App)
 
-
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
 app.use(router)
 app.use(pinia)
 
