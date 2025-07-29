a277253706ed7f76d50872ad6f6bf5e43d3bf26b
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-29 18:35:07 +0200

    router error

--- src/router/index.js
+++ src/router/index.js
@@ -26,7 +26,12 @@ const routes = [
  {
   path: '/',
   component: () => import('@/views/HomeRedirect.vue'),
-}
+},
+    {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue')
+  }
 ]
 
 export const router = createRouter({
