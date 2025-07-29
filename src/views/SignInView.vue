ae5f5c1c141895122565fb46e7922ea4793eba0d
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-29 17:16:18 +0200

    sign in style

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,9 +1,9 @@
-<script setup >
+<script setup>
 import { SignIn } from '@clerk/vue'
 </script>
 
-
 <template>
-
+  <div class="min-h-screen flex items-center justify-center">
     <SignIn />
+  </div>
 </template>
