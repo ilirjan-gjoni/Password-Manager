29ecd2d1d66eaca8d2b89daf4f827bd45b95ba32
Ilirjan Gjoni <ilirjangjoni@outlook.com>
2025-07-29 13:44:21 +0200

    router change

--- src/router/index.js
+++ src/router/index.js
@@ -1,45 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { SignIn, SignUp, useAuth } from '@clerk/vue'
 import { watch } from 'vue'
+import ListPasswords from '@/views/ListPasswords.vue'
+import AddPassword from '@/views/AddPassword.vue'
 
-//const requireAuth = (to, from, next) => {
-  //const { isSignedIn } = window.Clerk;
-  //  if (isSignedIn) {
-   // next();
-  //} else {
-   // next('/sign-in');
- // }
-//};
+/**
+ * Guard to protect routes: waits for Clerk to load, then
+ * allows signed-in users and redirects everyone else to /sign-in.
+ */
 const requireAuth = (to, from, next) => {
-const { isLoaded, isSignedIn } = useAuth() 
-if (!isLoaded.value) {
-  const stop = watch(isLoaded, (loaded) => {
-    if (loaded) {
-      stop()
-      requireAuth(to, from, next)
-    }
+  const { isLoaded, isSignedIn } = useAuth()
+
+  if (!isLoaded.value) {
+    const stop = watch(isLoaded, (loaded) => {
+      if (loaded) {
+        stop()
+        requireAuth(to, from, next)
+      }
+    })
+    return
+  }
+
+  if (isSignedIn.value) {
+    next()
+    return
+  }
+
+  next({
+    path: '/sign-in',
+    query: { redirect: to.fullPath }
   })
-  return
-}
-if (isSignedIn.value) {
-  next()
-  return
-}
-next({
-  path: '/sign-in',
-  query: { redirect: to.fullPath }
-})
 }
 
 const routes = [
   {
-    path: '/add',
-    component: () => import('@/views/AddPassword.vue'),
+    path: '/list',
+    component: ListPasswords,
     beforeEnter: requireAuth
   },
   {
-    path: '/list',
-    component: () => import('@/views/ListPasswords.vue'),
+    path: '/add',
+    component: AddPassword,
     beforeEnter: requireAuth
   },
   {
@@ -52,13 +53,27 @@ const routes = [
   },
   {
     path: '/',
-    redirect: () => {
-      const { isSignedIn } = useAuth()
-      return isSignedIn.value ? '/list' : '/sign-in'
-  }
+    /**
+     * Root guard: once Clerk loads, route to /list if signed in,
+     * otherwise to /sign-in.
+     */
+    beforeEnter: (to, from, next) => {
+      const { isLoaded, isSignedIn } = useAuth()
+
+      if (!isLoaded.value) {
+        const stop = watch(isLoaded, (loaded) => {
+          if (loaded) {
+            stop()
+            next(isSignedIn.value ? '/list' : '/sign-in')
+          }
+        })
+        return
+      }
+
+      next(isSignedIn.value ? '/list' : '/sign-in')
+    }
   }
 ]
-
 
 export const router = createRouter({
   history: createWebHistory(),
